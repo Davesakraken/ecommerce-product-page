5141cace083f9793566689e7f1012ef277505cce
Davesakraken <davidgeorgebell1995@outlook.com>
2023-07-21 19:21:23 +0100

    cart menu mobile positioning 2

--- src/components/Navbar/CartMenu.tsx
+++ src/components/Navbar/CartMenu.tsx
@@ -8,7 +8,7 @@ export default function CartMenu() {
   const containerLayout = "lg:w-[22.5rem] lg:h-[16rem] lg:translate-x-32";
 
   return (
-    <div className={`absolute flex flex-col top-24 right-[1vw] w-[98vw] ${containerLayout} ${containerStyles}`}>
+    <div className={`absolute flex flex-col top-24 right-[1vw] w-[98vw] h-[20rem] ${containerLayout} ${containerStyles}`}>
       <div className={`flex items-center ${cartMenuHeaderStyles}`}>Cart</div>
       <div className={`flex items-center justify-center text-center flex-auto ${cartEmptyStyles}`}>Your cart is empty.</div>
     </div>
