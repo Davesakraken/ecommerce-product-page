b6698504dd6f530ea950db16e9a5b6c9f84d98c1
Davesakraken <davidgeorgebell1995@outlook.com>
2023-07-23 23:36:52 +0100

    deploy fix

--- src/components/ItemListing/itemListing.tsx
+++ src/components/ItemListing/itemListing.tsx
@@ -1,7 +1,7 @@
 import ListingDetails from "./ListingDetails";
 import PictureGallery from "./PictureGallery";
 
-export default function itemListing() {
+export default function ItemListing() {
   return (
     <main>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-28 lg:mx-[24vw] mt-20">
--- src/components/Navbar/BurgerMenu.tsx
+++ src/components/Navbar/BurgerMenu.tsx
@@ -1,9 +1,6 @@
-// import { useState } from "react";
 import { MenuIcon } from "../Svgs";
 
 export default function BurgerMenu() {
-  // const [isOpen, setIsOpen] = useState(false);
-
   return (
     <>
       <MenuIcon classname="flex mt-[3px] mr-4 lg:hidden" />
