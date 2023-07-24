6a7405bb807a31b3e8187157901fa6ce514e011e
Davesakraken <davidgeorgebell1995@outlook.com>
2023-07-24 19:17:37 +0100

    maybe actual fix

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,5 @@
 import ItemListing from "./components/item_listing/item_listing";
-import NavBar from "./components/navbar/navbar";
+import NavBar from "./components/nav_bar/navbar";
 
 function App() {
   return (
