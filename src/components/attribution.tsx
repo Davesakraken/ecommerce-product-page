d3df13daf1d4ebbbc0c4f43db4b4867b37a570db
Davesakraken <davidgeorgebell1995@outlook.com>
2023-08-11 20:09:08 +0100

    github icon

--- src/components/attribution.tsx
+++ src/components/attribution.tsx
@@ -1,15 +1,24 @@
+import { GithubIcon } from "@/components/svgs";
+
 export default function Attribution() {
   return (
-    <div className="text-center my-3 py-3">
-      Challenge by{" "}
-      <a className="text-orange-default font-bold" href="https://www.frontendmentor.io?ref=challenge" target="_blank">
-        Frontend Mentor
-      </a>
-      . Coded by{" "}
-      <a className="text-orange-default font-bold" href="https://www.davidbell.xyz/" target="_blank">
-        David Bell
-      </a>
-      .
-    </div>
+    <section className="flex justify-center items-center p-5 gap-2">
+      <div>
+        Challenge by{" "}
+        <a className="text-orange-default font-bold" href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+          Frontend Mentor
+        </a>
+        . Coded by{" "}
+        <a className="text-orange-default font-bold" href="https://www.davidbell.xyz/" target="_blank">
+          David Bell
+        </a>
+        .
+      </div>
+      <div className="w-8">
+        <a href="https://github.com/Davesakraken/ecommerce-product-page/" target="_blank">
+          <GithubIcon additionalClassNames="text-black text-center" />
+        </a>
+      </div>
+    </section>
   );
 }
